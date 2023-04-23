55ab30b383836392a8b6ec44975cf782899d22c3
Igor Recio <igor.recio.h@gmail.com>
2023-04-23 21:56:07 +0200

    Removed example

--- aprs_traffic_eus/http_utils.py
+++ aprs_traffic_eus/http_utils.py
@@ -6,8 +6,4 @@ from dotenv import load_dotenv
 def get_XML_from_URL(url):
     r = requests.get(url, allow_redirects=True)
     open('IncidenciasTDT.xml', 'wb').write(r.content)
-
-
-load_dotenv()
-url = os.environ.get('URL')
-get_XML_from_URL(url)
+    
