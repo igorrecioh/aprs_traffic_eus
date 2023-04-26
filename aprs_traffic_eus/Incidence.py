111103165322202cebb65846c18cbefc2168e60b
Igor Recio <igor.recio.h@gmail.com>
2023-04-26 22:26:52 +0200

    Incidence class

---
+++ aprs_traffic_eus/Incidence.py
@@ -0,0 +1,16 @@
+class Incidence:
+    def __init__(self):
+        self.tipo = ""
+        self.autonomia = ""
+        self.provincia = ""
+        self.matricula = ""
+        self.causa = ""
+        self.poblacion = ""
+        self.fechahora_ini = ""
+        self.nivel = ""
+        self.carretera = ""
+        self.pk_inicial = ""
+        self.pk_final = ""
+        self.sentido = ""
+        self.longitud = 0.0
+        self.latitud = 0.0
