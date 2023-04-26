15e04290dabdb648af9cdb31984aee89e32f92e8
Igor Recio <igor.recio.h@gmail.com>
2023-04-26 22:26:25 +0200

    Improved coordinate conversion

--- aprs_traffic_eus/geo_utils.py
+++ aprs_traffic_eus/geo_utils.py
@@ -12,8 +12,8 @@ def dd2dm_raw(latitude, longitude):
     char_lat = ''
     char_long = ''
 
-    frac_lat, int_lat = math.modf(lat)
-    frac_long, int_long = math.modf(long)
+    frac_lat, int_lat = math.modf(float(latitude))
+    frac_long, int_long = math.modf(float(longitude))
 
     # Directions
     if int(int_lat) < 0:
@@ -35,8 +35,9 @@ def dd2dm_raw(latitude, longitude):
     mins_long = abs(frac_long) * 60
 
     # Round to 3 decimals
-    mins_lat = round(mins_lat,3)
-    mins_long = round(mins_long,3)
+    mins_lat = '%.3f'%(mins_lat)
+    mins_long = '%.3f'%(mins_long)
+
 
     lat_dm_str = str(degrees_lat) + "º" + str(mins_lat) + char_lat
     long_dm_str = str(degrees_long) + "º" + str(mins_long) + char_long
@@ -49,14 +50,18 @@ def dmraw2aprsformat(lat, long):
     lat_deg = lat.split('º')[0]
 
     lat_dec = lat.split('º')[1].split('.')[0]
-    #print(lat_dec)
     if int(lat_dec) < 10:
         lat_min_str = '0' + lat_dec
     else:
         lat_min_str = lat_dec
 
+    #print(lat_deg + lat_min_str)
     lat_min_dec = lat.split('º')[1].split('.')[1]
-    lat_aprs_str = lat_deg + lat_min_str + "." + lat_min_dec[0] + lat_min_dec[1] + lat_min_dec[3]
+    #print(lat_min_dec)
+    if len(lat_min_dec) == 4:
+        lat_aprs_str = lat_deg + lat_min_str + "." + lat_min_dec[0] + lat_min_dec[1] + lat_min_dec[3]
+    else:
+        lat_aprs_str = lat_deg + lat_min_str + "." + lat_min_dec[0] + lat_min_dec[1] + lat_min_dec[2]
 
     # Longitude parsing
     long_deg = long.split('º')[0]
@@ -70,21 +75,26 @@ def dmraw2aprsformat(lat, long):
 
     long_dec = long.split('º')[1].split('.')[0]
     long_min_dec = long.split('º')[1].split('.')[1]
-    long_aprs_str = long_deg_str + long_dec + "." + long_min_dec[0] + long_min_dec[1] + long_min_dec[3] 
 
+    if len(long_min_dec) == 4:
+        long_aprs_str = long_deg_str + long_dec + "." + long_min_dec[0] + long_min_dec[1] + long_min_dec[3]
+    else:
+        long_aprs_str = long_deg_str + long_dec + "." + long_min_dec[0] + long_min_dec[1] + long_min_dec[2]
+
+    print("APRS format: " + str(lat_aprs_str) + ", " + str(long_aprs_str))
     return lat_aprs_str, long_aprs_str
 
 
 
 
-lat = 43.0167
-long = -2.5412
+#lat = 43.4
+#long = -2.69543
 #Latitude = '4301.00N'
 #Longitude = '00232.47W'
-lat_dm, long_dm = dd2dm_raw(lat,long)
-print(lat_dm)
-print(long_dm)
+#lat_dm, long_dm = dd2dm_raw(lat,long)
+#print(lat_dm)
+#print(long_dm)
 
-lat_aprs, long_aprs = dmraw2aprsformat(lat_dm, long_dm)
-print(lat_aprs)
-print(long_aprs)
+#lat_aprs, long_aprs = dmraw2aprsformat(lat_dm, long_dm)
+#print(lat_aprs)
+#print(long_aprs)
